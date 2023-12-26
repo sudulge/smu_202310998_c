60dcd45f97b911e3935c4769224c65eab13a6831
sudulge <hhs0888@naver.com>
2023-12-26 19:49:32 +0900

    기말고사 2번 수정

--- projects/final/Project1/final2_202310998.c
+++ projects/final/Project1/final2_202310998.c
@@ -9,56 +9,101 @@ typedef struct {
 } Class;
 
 int main() {
+	int option;
+	int flag;
+	int run = 1;
 
 	Class class[5] = {
 		{"GPU프로그래밍", "전공심화", 4.84},
 		{"C프로그래밍1", "전공선택", 4.82},
 		{"인공지능과미래사회", "교양선택", 4.75},
 		{"파이썬프로그래밍", "전공선택", 4.65},
-		{"SW융합개론", "전공선택", 4.61},
+		{"SW융합개론", "전공선택", 4.61}
 	};
 
-	while (1) {
-		char input_class_name[50];
-		char input_class_category[50];
-		int option;
-		printf("=================\n");
-		printf("1) 과목명 검색, 2) 과목구분별 검색, 3) 강의평가점수로 검색, 다른 숫자) 종료 >\n");
-		printf("=================\n");
+	while (run) {
+		char input[50];
+		double input_score;
 
+		printf("1) 과목명 검색, 2) 과목구분별 검색, 3) 강의평가점수로 검색, 다른 숫자) 종료 >  ");
 		scanf("%d", &option);
 
 		switch (option) {
-		case 1:
-			printf("=================\n");
+		case(1):
+			flag = 0;
+
 			printf("과목명을 입력하세요> ");
-			scanf("%s", input_class_name);
+			scanf("%s", input);
+			/*fgets(input, sizeof(input), stdin);*/
 
 			for (int i = 0; i < 5; i++) {
-				if (!strcmp(&(&class[i])[0], input_class_name)) {
-					printf("%s 과목은 %s(으)로 구분되며, 강의평가점수는 %lf점입니다.", input_class_name, &((&class[i])[1]), (&class[i])[2]);
-					break;
+				if (!strcmp(class[i].class_name, input)) {
+					printf("%s 과목은 %s(으)로 구분되며, 강의평가 점수는 %.2lf점입니다.\n", class[i].class_name, class[i].class_category, class[i].class_score);
+					flag = 1;
 				}
 			}
-			break;
-		case 2:
-			scanf("%s", input_class_category);
 
-			if (!strcmp(input_class_category, "전공심화") || !strcmp(input_class_category, "전공선택") || !strcmp(input_class_category, "교양선택")) {
-				printf("%s 과목(들)은", input_class_category);
+			if (!flag) {
+				printf("%s 과목은 없습니다.\n", &input);
 			}
+
+			printf("\n");
+			break;
+
+		case(2):
+			flag = 0;
+
+			printf("과목구분을 입력하세요> ");
+			scanf("%s", input);
+
+			printf("%s 과목(들)은 ", input);
 
 			for (int i = 0; i < 5; i++) {
-				if (!strcmp(&(&class[i])[1], input_class_category)) {
-					printf(" %s(강의평가 %lf점),");
+				if (!strcmp(class[i].class_category, input)) {
+					printf("%s(강의평가 %.2lf점), ", class[i].class_name, class[i].class_score);
+					flag = 1;
 				}
 			}
-			printf("입니다.");
+
+			if (flag) {
+				printf("입니다.\n");
+			}
+			else {
+				printf("없습니다.\n");
+			}
+
+			printf("\n");
 			break;
-		case 3:
+
+		case(3):
+			flag = 0;
+
+			printf("강의평가점수 몇 점 이상의 과목을 검색하고자 하나요? > ");
+			scanf("%lf", &input_score);
+
+			printf("%.2lf점 이상의 강의평가점수를 받은 과목(들)은 ", input_score);
+
+			for (int i = 0; i < 5; i++) {
+				if (class[i].class_score >= input_score) {
+					printf("%s(%s, 강의평가 %.2lf점), ", class[i].class_name, class[i].class_category, class[i].class_score);
+					flag = 1;
+				}
+			}
+
+			if (flag) {
+				printf("입니다.\n");
+			}
+			else {
+				printf("없습니다.\n");
+			}
+
+			printf("\n");
+			break;
+
+		default:
+			printf("지금까지 S모대학교 N모교수의 강의과목 정보를 출력해 주는 프로그램이였습니다. 그럼 안녕히 계세요~");
+			run = 0;
 			break;
 		}
 	}
-
-	
 }
